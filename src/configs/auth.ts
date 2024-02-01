de4ed89bd9d3b2e2872f0eb74326884e8c889c4a
Kivanc Cakmak <kivanccakmak@gmail.com>
2024-02-02 02:42:49 +0300

    empric 5

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -1,7 +1,7 @@
 export default {
   meEndpoint: '/auth/me',
-  loginEndpoint: 'https://ciargyanclokbcragarw.supabase.co/functions/v1/rest/signin',
-  registerEndpoint: 'https://ciargyanclokbcragarw.supabase.co/functions/v1/rest/signup',
+  loginEndpoint: 'https://ciargyanclokbcragarw.supabase.co/functions/v1/hello-world/signin',
+  registerEndpoint: 'https://ciargyanclokbcragarw.supabase.co/functions/v1/hello-world/signup',
   storageTokenKeyName: 'accessToken',
   onTokenExpiration: 'refreshToken' // logout | refreshToken
 }
