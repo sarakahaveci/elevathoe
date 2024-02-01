0337d7c47e5ee74aca5b1be684f6815bf490a184
Kivanc Cakmak <kivanccakmak@gmail.com>
2024-02-02 02:29:29 +0300

    empiric 3

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -1,7 +1,7 @@
 export default {
   meEndpoint: '/auth/me',
-  loginEndpoint: 'https://ciargyanclokbcragarw.supabase.co/functions/v1/hello-world/signin',
-  registerEndpoint: '/rest/signup',
+  loginEndpoint: 'https://ciargyanclokbcragarw.supabase.co/functions/v1/rest/signin',
+  registerEndpoint: 'https://ciargyanclokbcragarw.supabase.co/functions/v1/rest/signup',
   storageTokenKeyName: 'accessToken',
   onTokenExpiration: 'refreshToken' // logout | refreshToken
 }
