9d2992b435fa0a253e89c66beb5f8ef5b049ccc7
Kivanc Cakmak <kivanccakmak@gmail.com>
2024-02-02 02:21:49 +0300

    empiric-2

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -1,6 +1,6 @@
 export default {
   meEndpoint: '/auth/me',
-  loginEndpoint: '/hello-world/signin',
+  loginEndpoint: 'https://ciargyanclokbcragarw.supabase.co/functions/v1/hello-world/signin',
   registerEndpoint: '/rest/signup',
   storageTokenKeyName: 'accessToken',
   onTokenExpiration: 'refreshToken' // logout | refreshToken
