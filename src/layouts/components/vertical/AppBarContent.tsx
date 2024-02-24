557f629b80386104d1f0ad8333c42a5795eba3cd
Kivanc Cakmak <kivanccakmak@gmail.com>
2024-02-24 18:05:10 +0300

    app-bar unused removals

--- src/layouts/components/vertical/AppBarContent.tsx
+++ src/layouts/components/vertical/AppBarContent.tsx
@@ -145,14 +145,11 @@ const AppBarContent = (props: Props) => {
             <Icon icon='mdi:menu' />
           </IconButton>
         ) : null}
-        {auth.user && <Autocomplete hidden={hidden} settings={settings} />}
       </Box>
       <Box className='actions-right' sx={{ display: 'flex', alignItems: 'center' }}>
-        <LanguageDropdown settings={settings} saveSettings={saveSettings} />
         <ModeToggler settings={settings} saveSettings={saveSettings} />
         {auth.user && (
           <>
-            <ShortcutsDropdown settings={settings} shortcuts={shortcuts} />
             <NotificationDropdown settings={settings} notifications={notifications} />
             <UserDropdown settings={settings} />
           </>
