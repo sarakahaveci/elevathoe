4b75f4db42550f3686d47ca5650daf64268589b2
Kivanc Cakmak <kivanccakmak@gmail.com>
2024-03-14 09:45:53 +0300

    cli.sh update

--- test/cli.sh
+++ test/cli.sh
@@ -20,28 +20,32 @@ fi
 	#-H "Authorization: Bearer $anonKey" \
 	#--data "{\"email\":\"$email\", \"password\": \"$password\"}" \
 	#| jq -r '.signInResponse.data.session.access_token')
+	#
+	#
 
-# forgot password
-api_url=$base_url"forgotPassword"
-curl -X POST  $api_url -H "apikey: $anonKey" -H "Content-Type: application/json" -H "Authorization: Bearer $anonKey" --data "{\"email\": \"$email\"}"
+#api_url=$base_url"signup"
+#curl -X POST  $api_url -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "{\"orgName\": \"myCompany-1\", \"email\": \"a@b.com\", \"password\": \"mypassword\"}"
+
+#api_url=$base_url"forgotPassword"
+#curl -X POST  $api_url -H "apikey: $anonKey" -H "Content-Type: application/json" -H "Authorization: Bearer $anonKey" --data "{\"email\": \"$email\"}"
 
 #api_url=$base_url"addCustomer"
 #curl -X POST  $api_url -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "{\"update\": 0, \"name\": \"burj el arab\", \"cancel\": 0}"
-#
+
 #api_url=$base_url"getCustomers"
 #curl -X POST  $api_url -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "{}"
 
 #api_url=$base_url"getUsers"
 #curl -X POST  $api_url -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "{}"
-#
+
 #api_url=$base_url"getProjects"
 #curl -X POST  $api_url -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "{}"
-#
+
 #api_url=$base_url"getMaintainers"
 #curl -X POST  $api_url -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "{}"
-#
+
 #api_url=$base_url"getElevators"
 #curl -X POST  $api_url -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "{}"
-#
+
 #api_url=$base_url"updatePassword"
 #curl -X POST  $api_url -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "{\"newPassword\": \"osman\"}"
