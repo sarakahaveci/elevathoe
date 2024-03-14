1e90b301784cfdc73106f68f900e59d7d30f6a33
Kivanc Cakmak <kivanccakmak@gmail.com>
2024-03-14 09:41:16 +0300

    cli update

--- test/cli.sh
+++ test/cli.sh
@@ -14,16 +14,16 @@ if [[ $run_local -eq 1 ]]; then
 fi
 
 # comment-out this part for /rest/signin, /rest/signup and /rest/forgotPassword
-token=$(curl -X POST $login_url \
-	-H "apikey: $anonKey" \
-	-H "Content-Type: application/json" \
-	-H "Authorization: Bearer $anonKey" \
-	--data "{\"email\":\"$email\", \"password\": \"$password\"}" \
-	| jq -r '.signInResponse.data.session.access_token')
+#token=$(curl -X POST $login_url \
+	#-H "apikey: $anonKey" \
+	#-H "Content-Type: application/json" \
+	#-H "Authorization: Bearer $anonKey" \
+	#--data "{\"email\":\"$email\", \"password\": \"$password\"}" \
+	#| jq -r '.signInResponse.data.session.access_token')
 
 # forgot password
-#api_url=$base_url"forgotPassword"
-#curl -X POST  $api_url -H "apikey: $anonKey" -H "Content-Type: application/json" -H "Authorization: Bearer $anonKey" --data "{\"email\": \"$email\"}"
+api_url=$base_url"forgotPassword"
+curl -X POST  $api_url -H "apikey: $anonKey" -H "Content-Type: application/json" -H "Authorization: Bearer $anonKey" --data "{\"email\": \"$email\"}"
 
 #api_url=$base_url"addCustomer"
 #curl -X POST  $api_url -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "{\"update\": 0, \"name\": \"burj el arab\", \"cancel\": 0}"
@@ -43,5 +43,5 @@ token=$(curl -X POST $login_url \
 #api_url=$base_url"getElevators"
 #curl -X POST  $api_url -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "{}"
 #
-api_url=$base_url"updatePassword"
-curl -X POST  $api_url -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "{\"newPassword\": \"osman\"}"
+#api_url=$base_url"updatePassword"
+#curl -X POST  $api_url -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "{\"newPassword\": \"osman\"}"
