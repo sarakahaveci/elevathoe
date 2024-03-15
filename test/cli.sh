f04e0b3fa9bbd567b838272e3265b180d7bc21ca
Kivanc Cakmak <kivanccakmak@gmail.com>
2024-03-15 22:41:23 +0300

    cli.sh update apis added

--- test/cli.sh
+++ test/cli.sh
@@ -14,14 +14,14 @@ if [[ $run_local -eq 1 ]]; then
 fi
 
 # comment-out this part for /rest/signin, /rest/signup and /rest/forgotPassword
-#token=$(curl -X POST $login_url \
-	#-H "apikey: $anonKey" \
-	#-H "Content-Type: application/json" \
-	#-H "Authorization: Bearer $anonKey" \
-	#--data "{\"email\":\"$email\", \"password\": \"$password\"}" \
-	#| jq -r '.signInResponse.data.session.access_token')
-	#
-	#
+token=$(curl -X POST $login_url \
+	-H "apikey: $anonKey" \
+	-H "Content-Type: application/json" \
+	-H "Authorization: Bearer $anonKey" \
+	--data "{\"email\":\"$email\", \"password\": \"$password\"}" \
+	| jq -r '.signInResponse.data.session.access_token')
+	
+	
 
 #api_url=$base_url"signup"
 #curl -X POST  $api_url -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "{\"orgName\": \"myCompany-1\", \"email\": \"a@b.com\", \"password\": \"mypassword\"}"
@@ -31,6 +31,9 @@ fi
 
 #api_url=$base_url"addCustomer"
 #curl -X POST  $api_url -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "{\"update\": 0, \"name\": \"burj el arab\", \"cancel\": 0}"
+#
+#api_url=$base_url"addProject"
+#curl -X POST  $api_url -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "{\"update\": 0, \"name\": \"Kristal Kule\", \"cancel\": 0, \"customerId\": \"74d3e1f8-d4b3-4e87-ab4f-cc6a04b1fdfb\", \"address\": \"4 Levent\"}"
 
 #api_url=$base_url"getCustomers"
 #curl -X POST  $api_url -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "{}"
