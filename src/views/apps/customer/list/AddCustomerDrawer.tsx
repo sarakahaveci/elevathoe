01ab29197aaa516b41849527f09e1c93b89638cc
sarakahveci <yamooonz@example.com>
2024-03-28 14:17:55 +0300

    unuseful code

--- src/views/apps/customer/list/AddCustomerDrawer.tsx
+++ src/views/apps/customer/list/AddCustomerDrawer.tsx
@@ -1,172 +1,168 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { useForm, Controller } from "react-hook-form";
-import { styled } from "@mui/material/styles";
-import Drawer from "@mui/material/Drawer";
-import TextField from "@mui/material/TextField";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import FormControl from "@mui/material/FormControl";
-import FormHelperText from "@mui/material/FormHelperText";
-import Button from "@mui/material/Button";
-import Icon from "src/@core/components/icon";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { RootState, AppDispatch } from "src/store";
+// ** React Imports
+import { useState } from 'react'
+import { useAuth } from 'src/hooks/useAuth'
 
-import { useAuth } from "src/hooks/useAuth";
+// ** MUI Imports
+import Drawer from '@mui/material/Drawer'
+import Select from '@mui/material/Select'
+import Button from '@mui/material/Button'
+import MenuItem from '@mui/material/MenuItem'
+import { styled } from '@mui/material/styles'
+import TextField from '@mui/material/TextField'
+import IconButton from '@mui/material/IconButton'
+import InputLabel from '@mui/material/InputLabel'
+import Typography from '@mui/material/Typography'
+import Box, { BoxProps } from '@mui/material/Box'
+import FormControl from '@mui/material/FormControl'
+import FormHelperText from '@mui/material/FormHelperText'
 
-interface SidebarAddCustomerProps {
-  open: boolean;
-  toggle: () => void;
-  updateCustomerList: (newCustomer: Customer) => void;
+// ** Third Party Imports
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm, Controller } from 'react-hook-form'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+
+// ** Store Imports
+import { useDispatch, useSelector } from 'react-redux'
+
+// ** Actions Imports
+import { addCustomer } from 'src/store/apps/customer'
+
+// ** Types Imports
+import { RootState, AppDispatch } from 'src/store'
+
+interface SidebarAddCustomerType {
+  open: boolean
+  toggle: () => void
+}
+
+interface CustomerData {
+  name: string
 }
 
 const showErrors = (field: string, valueLen: number, min: number) => {
   if (valueLen === 0) {
-    return `${field} field is required`;
+    return `${field} field is required`
   } else if (valueLen > 0 && valueLen < min) {
-    return `${field} must be at least ${min} characters`;
+    return `${field} must be at least ${min} characters`
   } else {
-    return "";
+    return ''
   }
-};
+}
 
-const Header = styled(Box)(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
+const Header = styled(Box)<BoxProps>(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
   padding: theme.spacing(3, 4),
-  justifyContent: "space-between",
-  backgroundColor: theme.palette.background.default,
-}));
+  justifyContent: 'space-between',
+  backgroundColor: theme.palette.background.default
+}))
 
 const schema = yup.object().shape({
   name: yup
     .string()
-    .min(3, (obj) => showErrors("First Name", obj.value.length, obj.min))
-    .required(),
-});
+    .min(3, obj => showErrors('First Name', obj.value.length, obj.min))
+    .required()
+})
 
-interface Customer {
-  name: string;
-}
 const defaultValues = {
-  name: "",
-};
+  name: ''
+}
 
-const SidebarAddCustomer = (props: SidebarAddCustomerProps) => {
-  const { open, toggle, updateCustomerList } = props;
-  const [newCustomer, setNewCustomer] = useState<Customer | null>(null);
+const SidebarAddCustomer = (props: SidebarAddCustomerType) => {
+  // ** Props
+  const { open, toggle } = props
+
+  // ** Hooks
+  const dispatch = useDispatch<AppDispatch>()
+  const store = useSelector((state: RootState) => state.customer)
   const {
     reset,
     control,
+    setValue,
     setError,
     handleSubmit,
-    formState: { errors },
+    formState: { errors }
   } = useForm({
     defaultValues,
-    mode: "onChange",
-    resolver: yupResolver(schema),
-  });
+    mode: 'onChange',
+    resolver: yupResolver(schema)
+  })
+  // const onSubmit = (data: CustomerData) => {
+  //   dispatch(addCustomer({ ...data }))
+  //   toggle()
+  //   reset()
+  // }
 
   const auth = useAuth();
-  
-  const onSubmit = (data: { name: string }) => {
+
+  // #api_url=$base_url"addCustomer"
+  // #curl -X POST  $api_url -H 
+  // "Content-Type: application/json" -H 
+  // "Authorization: Bearer $token" --data 
+  // "{\"update\": 0, \"name\": \"burj el arab\", \"cancel\": 0}"
+
+  const onSubmit = (data: CustomerData) => {
     const { name } = data;
     if (open) {
-      const newCustomerData: Customer = {
-        name: name
-      };
       auth.addcustomer({ name: name, cancel: 0, update: 0 }, () => {
-        props.updateCustomerList(newCustomerData);
-        console.log("New customer added:", newCustomerData);
-        reset();
+        // setError('name', {
+        //   type: 'manual',
+        //   message: 'Your input is incorrect',
+        // });
+
+        console.log('testing'); 
       });
-    
-      auth.getcustomer({ name }, (response) => {
-        console.log(response, 'testingData');
-        if (response.error) {
-          setError("name", {
-            type: "manual",
-            message: "Error: Unable to retrieve customer data",
-          });
-        } else {
-          console.log("Received customer data:", response.data);
-          }
-      });
-    };
-    
+    }
   }
-    
+
   const handleClose = () => {
-    toggle();
-    reset();
-  };
+    toggle()
+    reset()
+  }
 
   return (
     <Drawer
       open={open}
-      anchor="right"
-      variant="temporary"
+      anchor='right'
+      variant='temporary'
       onClose={handleClose}
       ModalProps={{ keepMounted: true }}
-      sx={{ "& .MuiDrawer-paper": { width: { xs: 300, sm: 400 } } }}
+      sx={{ '& .MuiDrawer-paper': { width: { xs: 300, sm: 400 } } }}
     >
       <Header>
-        <Typography variant="h6">Add Customer</Typography>
-        <IconButton
-          size="small"
-          onClick={handleClose}
-          sx={{ color: "text.primary" }}
-        >
-          <Icon icon="mdi:close" fontSize={20} />
+        <Typography variant='h6'>Add Customer</Typography>
+        <IconButton size='small' onClick={handleClose} sx={{ color: 'text.primary' }}>
+          <Icon icon='mdi:close' fontSize={20} />
         </IconButton>
       </Header>
       <Box sx={{ p: 5 }}>
         <form onSubmit={handleSubmit(onSubmit)}>
           <FormControl fullWidth sx={{ mb: 6 }}>
             <Controller
-              name="name"
+              name='name'
               control={control}
               rules={{ required: true }}
               render={({ field: { value, onChange } }) => (
-                <TextField
-                  value={value}
-                  label="Name"
-                  onChange={onChange}
-                  error={Boolean(errors.name)}
-                />
+                <TextField value={value} label='Name' onChange={onChange} error={Boolean(errors.name)} />
               )}
             />
-            {errors.name && (
-              <FormHelperText sx={{ color: "error.main" }}>
-                {errors.name.message}
-              </FormHelperText>
-            )}
+            {errors.name && <FormHelperText sx={{ color: 'error.main' }}>{errors.name.message}</FormHelperText>}
           </FormControl>
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <Button
-              size="large"
-              type="submit"
-              variant="contained"
-              sx={{ mr: 3 }}
-            >
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Button size='large' type='submit' variant='contained' sx={{ mr: 3 }}>
               Submit
             </Button>
-            <Button
-              size="large"
-              variant="outlined"
-              color="secondary"
-              onClick={handleClose}
-            >
+            <Button size='large' variant='outlined' color='secondary' onClick={handleClose}>
               Cancel
             </Button>
           </Box>
         </form>
       </Box>
     </Drawer>
-  );
-};
+  )
+}
 
-export default SidebarAddCustomer;
+export default SidebarAddCustomer
--- src/views/apps/customer/list/TableHeader.tsx
+++ src/views/apps/customer/list/TableHeader.tsx
@@ -2,9 +2,18 @@
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
-
+import { useAuth } from 'src/hooks/useAuth'
 // ** Icon Imports
 import Icon from 'src/@core/components/icon'
+
+
+// ** Third Party Imports
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm, Controller } from 'react-hook-form'
+import { RootState, AppDispatch } from 'src/store'
+// ** Store Imports
+import { useDispatch, useSelector } from 'react-redux'
 
 interface TableHeaderProps {
   value: string
@@ -15,6 +24,59 @@ interface TableHeaderProps {
 const TableHeader = (props: TableHeaderProps) => {
   // ** Props
   const { handleFilter, toggle, value } = props
+
+  interface SidebarAddCustomerType {
+    open: boolean
+    toggle: () => void
+  }
+  
+  interface CustomerData {
+    name: string
+  }
+  
+  const defaultValues = {
+    name: ''
+  }
+  interface FormData {
+name:string
+   }
+   
+    // ** Hooks
+    const dispatch = useDispatch<AppDispatch>()
+    const store = useSelector((state: RootState) => state.customer)
+    const {
+      reset,
+      control,
+      setValue,
+      setError,
+      handleSubmit,
+      formState: { errors }
+    } = useForm({
+      defaultValues,
+      mode: 'onChange',
+    })
+    // const onSubmit = (data: CustomerData) => {
+    //   dispatch(addCustomer({ ...data }))
+    //   toggle()
+    //   reset()
+    // }
+  
+    const auth = useAuth();
+    const onSubmit = (data: FormData) => {
+      const { name } = data
+      auth.getcustomer({ name}, () => {
+        setError('name', {
+          type: 'manual',
+          message: 'Email or Password is invalid'
+        })
+      })
+    }
+   
+    const handleClose = () => {
+      toggle()
+      reset()
+    }
+
 
   return (
     <Box sx={{ p: 5, pb: 3, display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-between' }}>
@@ -34,10 +96,12 @@ const TableHeader = (props: TableHeaderProps) => {
           placeholder='Search Customer'
           onChange={e => handleFilter(e.target.value)}
         />
-
+ <form onSubmit={handleSubmit(onSubmit)}>
+        
         <Button sx={{ mb: 2 }} onClick={toggle} variant='contained'>
           + Add
         </Button>
+        </form>
       </Box>
     </Box>
   )
