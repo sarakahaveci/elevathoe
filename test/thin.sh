cd53883864ad3cfd7973c7f86400d100059c8845
Kivanc Cakmak <kivanccakmak@gmail.com>
2024-04-29 09:40:39 +0300

    api added

--- test/thin.sh
+++ test/thin.sh
@@ -24,5 +24,8 @@ rm -rf output.json output.pdf
 #
 # add visit maintainer
 #python3 thin.py 'kivanccakmak@gmail.com' '987654321' 'POST' 'addVisitMaintainer' '{"maintainerId": "4c6a1881-5a45-4fe5-8c3a-fbac8b7c787a", "visitId": "04cea06b-5c6c-46e0-91cc-5cfe80e73cf0", "update": 0}'
+#
+# get customer projects
+#python3 thin.py 'kivanccakmak@gmail.com' '987654321' 'POST' 'getCustomerProjects' '{"customerId": "32c9c4f2-4a26-4c72-a8ee-db2b3a0e4e79", "start": 0, "finish": 10, "text": ""}'
 
 ./show.sh
